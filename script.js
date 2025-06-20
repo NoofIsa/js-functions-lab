6acf6f4244e68532cb57b6b981abf845d4f848cf
Noof <202102651@stu.uob.edu.bh>
2025-06-21 01:29:33 +0300

    Update

--- script.js
+++ script.js
@@ -61,7 +61,7 @@ function isCharAVowel (charvowel)
     return "fale"
 
 }
-console.log('Exercise 3 Result:', isCharAVowel("b"));
+console.log('Exercise 3 Result:', isCharAVowel("a"));
 
 /*
 Exercise 4: generateEmail()
@@ -97,22 +97,13 @@ Complete the exercise in the space below:
 function greetUser(name , time)
 
 {
-    if (time='morning')
-    {
-        return `Goodmorning${name}!`;
-    }
-    else if (time='afternoon')
-         {
-        return `Goodafternoon${name}!`;
-         }
-    else if (time='evening')
-         {
-        return `Goodevening${name}!`;
-         }
+
+    return`Good ${time} ${name}!`;
+    
         
     }
     
-console.log('Exercise 5 Result:', greetUser("Sam", "afternoon"));
+console.log('Exercise 5 Result:', greetUser("Sam", "morning"));
 
 /*
 Exercise 6: maxOfThree()
@@ -125,17 +116,107 @@ Example: maxOfThree(17, 4, 9) should return 17.
 Complete the exercise in the space below:
 */
 function maxOfThree(int1 ,int2 , int3 )
+
+{   let max;
+    if (int1>int2 && int1>int3)
+        max= int1;
+
+    else if (int2>int3)
+        max= int2;
+    else 
+         max= int3;
+
+return max;
+}
+
+console.log('Exercise 6 Result:', maxOfThree(17,4,9));
+
+/*
+Exercise 7: calculateTip()
+
+Create a function called calculateTip. It should take two arguments: 
+the bill amount and the tip percentage (as a whole number). 
+The function should return the amount of the tip.
+
+Example: calculateTip(50, 20) should return 10.
+
+Complete the exercise in the space below:
+*/
+
+function calculateTip(bill_amount ,tip_percentage )
 {
-    if (int1 >int2 )
-        {
+    return (tip_percentage*bill_amount)/100 ;
+}
+console.log('Exercise 7 Result:', calculateTip(50, 20));
 
 
-        }
+/*
+Exercise 8: convertTemperature()
+
+Write a function named convertTemperature. 
+It takes two arguments: a temperature and a string representing the 
+scale ('C' for Celsius, 'F' for Fahrenheit). 
+Convert the temperature to the other scale.
+
+Example: convertTemperature(32, 'C') should return 89.6 (Fahrenheit).
+Example: convertTemperature(32, 'F') should return 0 (Celsius).
+
+Complete the exercise in the space below:
+*/
+
+function convertTemperature(temperature,scale)
+{
+    if (scale=='C')
+    {
+        return (temperature * 9/5) + 32 + "fahrenheit";
+    }
+    else if (scale=='F')
+    {
+        return (temperature-32) *5/9 + "Celsius";
+    } 
+    else 
+    return ("invalide scale : use C or F ");
 
 }
 
-console.log('Exercise 4 Result:', generateEmail("johnsmith", "example.com"));
+
+console.log('Exercise 8 Result:', convertTemperature(32, "C"));
+
+/*
+Exercise 9: basicCalculator()
+
+Create a function named basicCalculator. 
+It should take three arguments: two numbers and a string representing 
+an operation ('add', 'subtract', 'multiply', 'divide'). 
+Perform the provided operation on the two numbers. 
+In operations where the order of numbers is important, 
+treat the first parameter as the first operand and the 
+second parameter as the second operand.
+
+Example: basicCalculator(10, 5, 'subtract') should return 5.
+
+Complete the exercise in the space below:
+*/
+ function basicCalculator (int_1 , int_2 , operator)
+ {
+    if (operator == "add" )
+        {
+            return int_1 + int_2;
+
+        }
+    else if (operator =="subtract")
+        {
+            return int_1 - int_2 ; 
+        }
+    else if (operator == "multiply")
+        {
+            return int_1 * int_2 ;
+        }
+    else if ( operator == "divide" )
+        return int_1 / int_2 ;     
+    else 
+        return ("The operator was wrong: chose from (add ,subtract ,multiply ,divide ) ");
+ }
 
 
-
-console.log('Exercise 6 Result:', maxOfThree(5, 10, 8));
+console.log('Exercise 9 Result:', basicCalculator(10, 5, "subtract"));
